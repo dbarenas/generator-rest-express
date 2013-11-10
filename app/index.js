3cb1dfe0d9d82e6c75682131bb059a2ae80feed3
Brian Viveiros <brianlovescode@gmail.com>
2013-11-09 20:03:00 -0500

    added gitignore file

--- app/index.js
+++ app/index.js
@@ -67,5 +67,6 @@ ExpressRestGenerator.prototype.app = function app() {
 ExpressRestGenerator.prototype.projectfiles = function projectfiles() {
 	this.copy('jshintrc', '.jshintrc');
 	this.copy('travis.yml', '.travis.yml');
+	this.copy('gitignore', ".gitignore");
 };
 
